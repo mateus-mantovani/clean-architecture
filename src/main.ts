96e922dafe71440cb4b9af2c9102c9eb4bc9fb15
Mateus Mantovani <mateus21mantovani@hotmail.com>
2022-06-12 18:54:44 -0300

    feat: add product service

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import Customer from './entity/customer'
 import Order from './entity/order'
 import OrderItem from './entity/order_item'
 
-const customer = new Customer('123', 'John', '123 Main St')
+const customer = new Customer('123', 'John')
 const address = new Address('123 Main St', 123, '12345', 'Anytown')
 customer.address = address
 customer.activate()
 
-const item1 = new OrderItem(1, 'Item 1', 10)
-const item2 = new OrderItem(2, 'Item 2', 20)
+const item1 = new OrderItem(1, 'Item 1', 10, 'p1', 2)
+const item2 = new OrderItem(2, 'Item 2', 20, 'p2', 3)
 
 const order = new Order(1, '123', [item1, item2])

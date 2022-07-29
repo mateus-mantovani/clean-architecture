1ede711641475cd922f40a36dd25d00898c94569
Mateus Mantovani <mateus21mantovani@hotmail.com>
2022-07-28 22:46:43 -0300

    feat: implements customer domain events

---
+++ src/domain/event/customer/customer-address-changed.event.spec.ts
@@ -0,0 +1,27 @@
+import EventDispatcher from '../@shared/event-dispatcher'
+import CustomerAddressChangedEvent from './customer-address-changed.event'
+import Action1WhenCustomerAddressChangedEventHandler from './handler/action1-when-customer-address-changed.handle'
+
+describe('Customer address changed event', () => {
+  it('should notify all event handlers', () => {
+    const eventDispatcher = new EventDispatcher()
+    const eventHandler = new Action1WhenCustomerAddressChangedEventHandler()
+
+    eventDispatcher.register('CustomerAddressChangedEvent', eventHandler)
+
+    const spyEventHandler = jest.spyOn(eventHandler, 'handle')
+
+    expect(eventDispatcher.getEventHandlers.CustomerAddressChangedEvent).toBeDefined()
+    expect(eventDispatcher.getEventHandlers.CustomerAddressChangedEvent.length).toBe(1)
+    expect(eventDispatcher.getEventHandlers.CustomerAddressChangedEvent[0]).toMatchObject(eventHandler)
+
+    const customerAddressChangedEvent = new CustomerAddressChangedEvent({
+      id: 1,
+      name: 'Customer 1',
+      address: 'Rua 1'
+    })
+
+    eventDispatcher.notify(customerAddressChangedEvent)
+    expect(spyEventHandler).toHaveBeenCalled()
+  })
+})
---
+++ src/domain/event/customer/customer-created.event.spec.ts
@@ -0,0 +1,32 @@
+import EventDispatcher from '../@shared/event-dispatcher'
+import CustomerCreatedEvent from './customer-created.event'
+import Action1WhenCustomerIsCreatedEventHandler from './handler/action1-when-customer-is-created.handler'
+import Action2WhenCustomerIsCreatedEventHandler from './handler/action2-when-customer-is-created.handler'
+
+describe('Customer created event', () => {
+  it('should notify all event handlers', () => {
+    const eventDispatcher = new EventDispatcher()
+    const eventHandler1 = new Action1WhenCustomerIsCreatedEventHandler()
+    const eventHandler2 = new Action2WhenCustomerIsCreatedEventHandler()
+
+    eventDispatcher.register('CustomerCreatedEvent', eventHandler1)
+    eventDispatcher.register('CustomerCreatedEvent', eventHandler2)
+
+    const spyEventHandler1 = jest.spyOn(eventHandler1, 'handle')
+    const spyEventHandler2 = jest.spyOn(eventHandler2, 'handle')
+
+    expect(eventDispatcher.getEventHandlers.CustomerCreatedEvent).toBeDefined()
+    expect(eventDispatcher.getEventHandlers.CustomerCreatedEvent.length).toBe(2)
+    expect(eventDispatcher.getEventHandlers.CustomerCreatedEvent[0]).toMatchObject(eventHandler1)
+    expect(eventDispatcher.getEventHandlers.CustomerCreatedEvent[1]).toMatchObject(eventHandler2)
+
+    const customerCreatedEvent = new CustomerCreatedEvent({
+      name: 'Customer 1',
+      email: 'xpto@email.com'
+    })
+
+    eventDispatcher.notify(customerCreatedEvent)
+    expect(spyEventHandler1).toHaveBeenCalled()
+    expect(spyEventHandler2).toHaveBeenCalled()
+  })
+})
